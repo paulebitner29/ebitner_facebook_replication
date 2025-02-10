32d8c27f3bc210e6b576a9660212f75663c2e8a9
Paul Ebitner <131864803+paulebitner29@users.noreply.github.com>
2025-02-10 22:28:11 +0800

    Update home_screen.dart

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,58 +1,72 @@
-import 'package:facebook_replication/constants.dart';
-import 'package:facebook_replication/screens/notification_screen.dart';
-import 'package:facebook_replication/screens/profile_screen.dart';
+import '/screens/notification_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import '/constants.dart';
 import '../screens/newsfeed_screen.dart';
 import '../widgets/customfont.dart';
+import '/screens/profile_screen.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+  final String username;
+  const HomeScreen({super.key, required this.username});
 
   @override
-  State<HomeScreen> createState() => _HomeScreen();
+  State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _HomeScreen extends State<HomeScreen> {
+class _HomeScreenState extends State<HomeScreen> {
   int _selectedIndex = 0;
   final PageController _pageController = PageController();
+  String? _username;
+
+  Future<void> _loadUsername() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {});
+    print('Retrieved Username: $_username');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _username = widget.username; // Set the username from the constructor
+    _loadUsername(); // Optional: Load persisted username if needed
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        shadowColor: FB_TEXT_COLOR_WHITE,
+        shadowColor: FB_LIGHT_PRIMARY,
         elevation: 2,
         title: CustomFont(
           text: _selectedIndex == 0
-              ? 'PolBook'
+              ? 'Polbook'
               : _selectedIndex == 1
-                  ? 'notification'
-                  : 'Paul Ebitner',
+                  ? 'Notifications'
+                  : widget.username,
           fontSize: ScreenUtil().setSp(25),
           color: _selectedIndex == 0
-          ? FB_SECONDARY
-          : const Color.fromARGB(255, 0, 0, 0),
+              ? FB_SECONDARY
+              : const Color.fromARGB(255, 0, 0, 0),
           fontFamily: 'Klavika',
         ),
-        actions: [
-          TextButton(onPressed: () => print('LOGOUT'), child: Text('LOGOUT')),
-        ],
       ),
       body: PageView(
-          controller: _pageController,
-          children: const <Widget>[
-            NewsfeedScreen(),
-            NotificationScreen(),
-            ProfileScreen(),
-          ],
-          onPageChanged: (page) {
-            setState(
-              () {
-                _selectedIndex = page;
-              },
-            );
-          }),
+        controller: _pageController,
+        children: <Widget>[
+          const NewsfeedScreen(),
+          const NotificationScreen(),
+          ProfileScreen(
+            username: widget.username,
+          ),
+        ],
+        onPageChanged: (page) {
+          setState(() {
+            _selectedIndex = page;
+          });
+        },
+      ),
       bottomNavigationBar: BottomNavigationBar(
         showSelectedLabels: false,
         showUnselectedLabels: false,
@@ -63,7 +77,7 @@ class _HomeScreen extends State<HomeScreen> {
               icon: Icon(Icons.notifications), label: 'Notifications'),
           BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
         ],
-        selectedItemColor: FB_SECONDARY,
+        selectedItemColor: FB_PRIMARY,
         currentIndex: _selectedIndex,
       ),
     );
