e5735cc716ede28ef106aab08f2be546c902a94b
Paul Ebitner <131864803+paulebitner29@users.noreply.github.com>
2025-02-10 22:21:49 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,36 @@
-import 'package:facebook_replication/screens/home_screen.dart';
-import 'package:facebook_replication/screens/newsfeed_screen.dart';
-import 'package:facebook_replication/screens/profile_screen.dart';
+import '/screens/login_screen.dart';
+import '/screens/notification_screen.dart';
+import '/screens/register_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
- 
-void main() {
+import '../screens/home_screen.dart';
+import '../screens/newsfeed_screen.dart';
+
+void main()  {
   runApp(const FacebookReplication());
 }
- 
+
 class FacebookReplication extends StatelessWidget {
   const FacebookReplication({super.key});
- 
+
   @override
- Widget build(BuildContext context) {
+  Widget build(BuildContext context) {
     return ScreenUtilInit(
       designSize: const Size(412, 715),
       minTextAdapt: true,
       splitScreenMode: true,
-      builder: (context, child) {
+      builder: (_, child) {
         return MaterialApp(
           debugShowCheckedModeBanner: false,
           title: 'Facebook Replication',
-          initialRoute: '/home',
+          initialRoute: '/login',
           routes: {
+            '/login': (context) => const LoginScreen(),
+            '/register': (context) => const RegisterScreen(),
             '/newsfeed': (context) => const NewsfeedScreen(),
-            '/home': (context) => const HomeScreen(),
-            '/profilescreen': (context) => const ProfileScreen(),
+            '/home': (context) => const HomeScreen(username: '',),
+            '/notification': (context) => const NotificationScreen(),
+
           },
         );
       },
