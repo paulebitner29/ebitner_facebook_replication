f1384a2ebb200570d9aade2a89e2f08f40321ad4
Paul Ebitner <131864803+paulebitner29@users.noreply.github.com>
2025-02-14 01:15:48 +0800

    Update profile_photos.dart

--- lib/widgets/profile_photos.dart
+++ lib/widgets/profile_photos.dart
@@ -1,3 +1,4 @@
+import 'package:facebook_replication/widgets/custom_dialogs.dart';
 import 'package:flutter/material.dart';
 
 class profile_photos extends StatelessWidget {
@@ -16,81 +17,171 @@ class profile_photos extends StatelessWidget {
       crossAxisCount: 3,
       children: <Widget>[
         Container(
-          padding: const EdgeInsets.all(0),
-          child:
-              Image.network('assets/images/paulebitner.jpg', fit: BoxFit.cover),
-        ),
+            padding: const EdgeInsets.all(0),
+            child: InkWell(
+              onTap: () => customShowImageDialog(context,
+                  imageUrl:
+                      'https://cf.bstatic.com/xdata/images/hotel/max1024x768/486566798.jpg?k=a2a38bd1dfe49284e24eb1acfdadc4dffa72a261374ab5ec6ca9ed84ca7f74b0&o=&hp=1'),
+              child: Image.network(
+                  'https://cf.bstatic.com/xdata/images/hotel/max1024x768/486566798.jpg?k=a2a38bd1dfe49284e24eb1acfdadc4dffa72a261374ab5ec6ca9ed84ca7f74b0&o=&hp=1',
+                  fit: BoxFit.cover),
+            )),
         Container(
-          padding: const EdgeInsets.all(0),
-          child:
-              Image.network('assets/images/paulebitner.jpg', fit: BoxFit.cover),
-        ),
+            padding: const EdgeInsets.all(0),
+            child: InkWell(
+              onTap: () => customShowImageDialog(context,
+                  imageUrl:
+                      'https://cf.bstatic.com/xdata/images/hotel/max1024x768/486566798.jpg?k=a2a38bd1dfe49284e24eb1acfdadc4dffa72a261374ab5ec6ca9ed84ca7f74b0&o=&hp=1'),
+              child: Image.network(
+                  'https://cf.bstatic.com/xdata/images/hotel/max1024x768/486566798.jpg?k=a2a38bd1dfe49284e24eb1acfdadc4dffa72a261374ab5ec6ca9ed84ca7f74b0&o=&hp=1',
+                  fit: BoxFit.cover),
+            )),
         Container(
-          padding: const EdgeInsets.all(0),
-          child:
-              Image.network('assets/images/paulebitner.jpg', fit: BoxFit.cover),
-        ),
+            padding: const EdgeInsets.all(0),
+            child: InkWell(
+              onTap: () => customShowImageDialog(context,
+                  imageUrl:
+                      'https://cf.bstatic.com/xdata/images/hotel/max1024x768/486566798.jpg?k=a2a38bd1dfe49284e24eb1acfdadc4dffa72a261374ab5ec6ca9ed84ca7f74b0&o=&hp=1'),
+              child: Image.network(
+                  'https://cf.bstatic.com/xdata/images/hotel/max1024x768/486566798.jpg?k=a2a38bd1dfe49284e24eb1acfdadc4dffa72a261374ab5ec6ca9ed84ca7f74b0&o=&hp=1',
+                  fit: BoxFit.cover),
+            )),
         Container(
-          padding: const EdgeInsets.all(0),
-          child:
-              Image.network('assets/images/pepitopost.jpg', fit: BoxFit.cover),
-        ),
+            padding: const EdgeInsets.all(0),
+            child: InkWell(
+              onTap: () => customShowImageDialog(context,
+                  imageUrl: 'assets/images/paulebitner.jpg'),
+              child: Image.network('assets/images/paulebitner.jpg',
+                  fit: BoxFit.cover),
+            )),
         Container(
-          padding: const EdgeInsets.all(0),
-          child:
-              Image.network('assets/images/pepitopost.jpg', fit: BoxFit.cover),
-        ),
+            padding: const EdgeInsets.all(0),
+            child: InkWell(
+              onTap: () => customShowImageDialog(context,
+                  imageUrl: 'assets/images/paulebitner.jpg'),
+              child: Image.network('assets/images/paulebitner.jpg',
+                  fit: BoxFit.cover),
+            )),
         Container(
-          padding: const EdgeInsets.all(0),
-          child:
-              Image.network('assets/images/pepitopost.jpg', fit: BoxFit.cover),
-        ),
+            padding: const EdgeInsets.all(0),
+            child: InkWell(
+              onTap: () => customShowImageDialog(context,
+                  imageUrl: 'assets/images/paulebitner.jpg'),
+              child: Image.network('assets/images/paulebitner.jpg',
+                  fit: BoxFit.cover),
+            )),
         Container(
-          padding: const EdgeInsets.all(0),
-          child:
-              Image.network('assets/images/pepitopost.jpg', fit: BoxFit.cover),
-        ),
+            padding: const EdgeInsets.all(0),
+            child: InkWell(
+              onTap: () => customShowImageDialog(context,
+                  imageUrl: 'assets/images/pepitopost.jpg'),
+              child: Image.network('assets/images/pepitopost.jpg',
+                  fit: BoxFit.cover),
+            )),
         Container(
-          padding: const EdgeInsets.all(0),
-          child: Image.network('assets/images/boracay.jpg', fit: BoxFit.cover),
-        ),
+            padding: const EdgeInsets.all(0),
+            child: InkWell(
+              onTap: () => customShowImageDialog(context,
+                  imageUrl: 'assets/images/pepitopost.jpg'),
+              child: Image.network('assets/images/pepitopost.jpg',
+                  fit: BoxFit.cover),
+            )),
         Container(
-          padding: const EdgeInsets.all(0),
-          child: Image.network('assets/images/boracay.jpg', fit: BoxFit.cover),
-        ),
+            padding: const EdgeInsets.all(0),
+            child: InkWell(
+              onTap: () => customShowImageDialog(context,
+                  imageUrl: 'assets/images/pepitopost.jpg'),
+              child: Image.network('assets/images/pepitopost.jpg',
+                  fit: BoxFit.cover),
+            )),
         Container(
-          padding: const EdgeInsets.all(0),
-          child: Image.network('assets/images/boracay.jpg', fit: BoxFit.cover),
-        ),
+            padding: const EdgeInsets.all(0),
+            child: InkWell(
+              onTap: () => customShowImageDialog(context,
+                  imageUrl: 'assets/images/pepitopost.jpg'),
+              child: Image.network('assets/images/pepitopost.jpg',
+                  fit: BoxFit.cover),
+            )),
         Container(
-          padding: const EdgeInsets.all(0),
-          child: Image.network('assets/images/boracay.jpg', fit: BoxFit.cover),
-        ),
+            padding: const EdgeInsets.all(0),
+            child: InkWell(
+              onTap: () => customShowImageDialog(context,
+                  imageUrl: 'assets/images/boracay.jpg'),
+              child:
+                  Image.network('assets/images/boracay.jpg', fit: BoxFit.cover),
+            )),
         Container(
-          padding: const EdgeInsets.all(0),
-          child:
-              Image.network('assets/images/jessicapost.png', fit: BoxFit.cover),
-        ),
+            padding: const EdgeInsets.all(0),
+            child: InkWell(
+              onTap: () => customShowImageDialog(context,
+                  imageUrl: 'assets/images/boracay.jpg'),
+              child:
+                  Image.network('assets/images/boracay.jpg', fit: BoxFit.cover),
+            )),
         Container(
-          padding: const EdgeInsets.all(0),
-          child:
-              Image.network('assets/images/jessicapost.png', fit: BoxFit.cover),
-        ),
+            padding: const EdgeInsets.all(0),
+            child: InkWell(
+              onTap: () => customShowImageDialog(context,
+                  imageUrl: 'assets/images/boracay.jpg'),
+              child:
+                  Image.network('assets/images/boracay.jpg', fit: BoxFit.cover),
+            )),
         Container(
-          padding: const EdgeInsets.all(0),
-          child:
-              Image.network('assets/images/jessicapost.png', fit: BoxFit.cover),
-        ),
+            padding: const EdgeInsets.all(0),
+            child: InkWell(
+              onTap: () => customShowImageDialog(context,
+                  imageUrl: 'assets/images/boracay.jpg'),
+              child:
+                  Image.network('assets/images/boracay.jpg', fit: BoxFit.cover),
+            )),
         Container(
-          padding: const EdgeInsets.all(0),
-          child:
-              Image.network('assets/images/jessicapost.png', fit: BoxFit.cover),
-        ),
+            padding: const EdgeInsets.all(0),
+            child: InkWell(
+              onTap: () => customShowImageDialog(context,
+                  imageUrl: 'assets/images/jessicapost.png'),
+              child: Image.network('assets/images/jessicapost.png',
+                  fit: BoxFit.cover),
+            )),
         Container(
-          padding: const EdgeInsets.all(0),
-          child:
-              Image.network('assets/images/jessicapost.png', fit: BoxFit.cover),
-        ),
+            padding: const EdgeInsets.all(0),
+            child: InkWell(
+              onTap: () => customShowImageDialog(context,
+                  imageUrl: 'assets/images/jessicapost.png'),
+              child: Image.network('assets/images/jessicapost.png',
+                  fit: BoxFit.cover),
+            )),
+        Container(
+            padding: const EdgeInsets.all(0),
+            child: InkWell(
+              onTap: () => customShowImageDialog(context,
+                  imageUrl: 'assets/images/jessicapost.png'),
+              child: Image.network('assets/images/jessicapost.png',
+                  fit: BoxFit.cover),
+            )),
+        Container(
+            padding: const EdgeInsets.all(0),
+            child: InkWell(
+              onTap: () => customShowImageDialog(context,
+                  imageUrl: 'assets/images/jessicapost.png'),
+              child: Image.network('assets/images/jessicapost.png',
+                  fit: BoxFit.cover),
+            )),
+        Container(
+            padding: const EdgeInsets.all(0),
+            child: InkWell(
+              onTap: () => customShowImageDialog(context,
+                  imageUrl: 'assets/images/jessicapost.png'),
+              child: Image.network('assets/images/jessicapost.png',
+                  fit: BoxFit.cover),
+            )),
+        Container(
+            padding: const EdgeInsets.all(0),
+            child: InkWell(
+              onTap: () => customShowImageDialog(context,
+                  imageUrl: 'assets/images/jessicapost.png'),
+              child: Image.network('assets/images/jessicapost.png',
+                  fit: BoxFit.cover),
+            )),
       ],
     );
   }
